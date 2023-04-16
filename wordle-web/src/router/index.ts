1f4f0e05486d322b412538b60218caee232feaec
VeraKorchemnaya <vkorchemnaya@ewu.edu>
2023-04-16 11:00:34 -0700

    Unstage

--- wordle-web/src/router/index.ts
+++ wordle-web/src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import Wordle from '../views/WordleView.vue'
+import Wordle from '../views/Wordle.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
