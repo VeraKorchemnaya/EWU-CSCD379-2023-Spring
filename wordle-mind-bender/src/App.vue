01c048a79428b7bf5affda15c446c2cbf46459f1
AndrewKorchemniy <andrewkorch@gmail.com>
2023-04-16 13:49:52 -0700

    Adding settings dialog

--- wordle-mind-bender/src/App.vue
+++ wordle-mind-bender/src/App.vue
@@ -29,6 +29,35 @@ import { RouterView } from 'vue-router'
                 </v-list-item>
               </v-list>
             </v-menu>
+              
+            <v-dialog
+              transition="dialog-bottom-transition"
+              width="auto"
+            >
+              <template v-slot:activator="{ props }">
+                <v-btn
+                  v-bind="props"
+                ><v-icon icon="mdi-cog" size="x-large"></v-icon></v-btn>
+              </template>
+              <template v-slot:default="{ isActive }">
+                <v-card>
+                  <v-toolbar
+                    title="Settings"
+                  ></v-toolbar>
+                  <v-card-text>
+                    <div class="text-h2 pa-12">Hello world!</div>
+                  </v-card-text>
+                  <v-card-actions class="justify-end">
+                    <v-btn
+                      variant="text"
+                      @click="isActive.value = false"
+                    >Close</v-btn>
+                  </v-card-actions>
+                </v-card>
+              </template>
+            </v-dialog>
+
+
           </v-toolbar>
         </v-card>
       </v-col>
@@ -40,3 +69,4 @@ import { RouterView } from 'vue-router'
     </v-row>
   </v-container>
 </template>
+
