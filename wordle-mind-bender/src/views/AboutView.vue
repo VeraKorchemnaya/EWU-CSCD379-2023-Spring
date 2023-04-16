7aae2624e10bf94d12b962dc1684a7dd1e5c6290
AndrewKorchemniy <andrewkorch@gmail.com>
2023-04-16 13:41:06 -0700

    Adding back button to about page

--- wordle-mind-bender/src/views/AboutView.vue
+++ wordle-mind-bender/src/views/AboutView.vue
@@ -6,6 +6,9 @@
         <v-card-text>
             Welcome to our Wordle game app! This project was created for a class assignment and showcases our skills in Vue.js and Veutify. I hope you enjoy playing the game and exploring the different features, including the ability to customize the color scheme and view available words. Thank you for visiting and have fun! Meg is a wonderful person!
         </v-card-text>
+        <v-card-actions>
+            <v-btn @click="$router.go(-1)">Back</v-btn>
+        </v-card-actions>
       </v-card>
     </div>
   </main>
