56c07554036867d61b5f7bfa14f4220f540b112f
VeraKorchemnaya <vkorchemnaya@ewu.edu>
2023-04-16 19:24:18 -0700

    Style Tweaks

--- wordle-mind-bender/src/views/Wordle.vue
+++ wordle-mind-bender/src/views/Wordle.vue
@@ -1,31 +1,46 @@
 <template>
-    <h1>Wordle Mind Bender</h1>
-    <v-text-field v-model="guess" label="Guess" variant="solo"></v-text-field>
-  
-    <v-btn @click="checkGuess">Check</v-btn>
-    <div>
-      <v-row v-for="word in game.guesses" :key="word.text">
-        <v-col v-for="letter in word.letters" :key="letter.char">
-          <v-btn :color="letter.color">
-            {{ letter.char }}
-          </v-btn>
-        </v-col>
-      </v-row>
-    </div>
-  
-    <h2>{{ guess }}</h2>
-    <h3>{{ game.secretWord }}</h3>
-  </template>
-  
-  <script setup lang="ts">
-  import { WordleGame } from '@/scripts/wordleGame'
-  import { ref, reactive } from 'vue'
-  
-  const guess = ref('')
-  const game = reactive(new WordleGame())
-  console.log(game.secretWord)
-  
-  function checkGuess() {
-    game.submitGuess(guess.value)
-  }
-  </script>
+  <v-row>
+    <v-col>
+      <v-card flat class="mx-auto" style="width: 500px">
+        <v-card-title class="text-center text-h4 font-weight-bold">Wordle Mind Bender</v-card-title>
+        <v-card-text>
+          <v-row v-for="word in game.guesses" :key="word.text">
+            <v-col v-for="letter in word.letters" :key="letter.char">
+              <v-btn class="ps-0 pe-0" :color="letter.color" flat>
+                {{ letter.char }}
+              </v-btn>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col>
+              <v-text-field
+                variant="solo"
+                v-model="guess"
+                placeholder="Guess"
+                maxlength="5"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn variant="tonal" @click="checkGuess">Check</v-btn>
+          <v-btn disabled>Secret: {{ game.secretWord }}</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script setup lang="ts">
+import { WordleGame } from '@/scripts/wordleGame'
+import { ref, reactive } from 'vue'
+
+const guess = ref('')
+const game = reactive(new WordleGame())
+console.log(game.secretWord)
+
+function checkGuess() {
+  game.submitGuess(guess.value)
+}
+</script>
